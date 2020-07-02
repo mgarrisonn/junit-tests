21e37c2e50de481c046955074aafd387cf191320
michael garrison <mgarrison.sa@gmail.com>
2020-07-01 19:56:45 -0500

    Student class

---
+++ src/main/java/Student.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name){
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+
+}
